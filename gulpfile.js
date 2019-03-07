15592a51cac73f283bc570067cb0211f59640df4
Manoel Rodrigo Farinha de Medeiros <manorfm@hotmail.com>
2019-03-07 12:19:22 -0300

    fixing the changes of updating the gulp vesion

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,36 @@ const nodemon = require('gulp-nodemon');
 const jshint = require('gulp-jshint');
 const mocha = require('gulp-mocha');
 
-gulp.task('default', ['nodemon'], () => { });
-
-gulp.task('test', ['lint'], () =>
-	gulp.src(['./test/**/*.js'], {read: false})
-		.pipe(mocha({timeout: 3000, recursive: true, exit: true}))
-		.on('error', console.error));
-
-gulp.task('lint', () =>
-  gulp.src(['./api/**/*.js', './config/**/*.js'])
-      .pipe(jshint('.jshintrc'))
-      .pipe(jshint.reporter('jshint-stylish'))
+gulp.task('lint', 
+    gulp.series(() =>
+        gulp.src(['./api/**/*.js', './config/**/*.js'])
+            .pipe(jshint('.jshintrc'))
+            .pipe(jshint.reporter('jshint-stylish')
+        )
+    )
 );
 
-gulp.task('nodemon', (cb) => {
-    var started = false;
-    return nodemon({ script: 'loader.js' })
-        .on('start', () => {
-            if (!started) {
-                cb();
-                started = true;
-            }
-        });
-});
+gulp.task('test', 
+    gulp.series('lint', gulp.parallel(() =>
+        gulp.src(['./test/**/**.spec.js'], {read: false})
+            .pipe(mocha({timeout: 3000, recursive: true, exit: true}))
+            .on('error', console.error),
+        // gulp.src('./test/**/*.jest.js')
+        //     .pipe(jest({"preprocessorIgnorePatterns": [ "<rootDir>/node_modules/"], "automock": false }))
+    ))
+);
+
+gulp.task('nodemon', 
+    gulp.series((cb) => {
+        var started = false;
+        return nodemon({ script: 'loader.js' })
+            .on('start', () => {
+                if (!started) {
+                    cb();
+                    started = true;
+                }
+            });
+    })
+);
+
+gulp.task('default', gulp.series('nodemon', () => { }));
